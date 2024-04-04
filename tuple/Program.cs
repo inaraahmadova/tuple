fa0a803515c10baef4e92333128c9c49832ec49f
inaraahmadova <tu7r5hs82@code.edu.az>
2024-04-04 23:13:08 +0400

    first commit

---
+++ tuple/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = A();
+            Console.WriteLine(result.Item1);
+
+            var result2 = B();
+            Console.WriteLine(result2);
+
+            var result3 = C();
+            Console.WriteLine(result3.Item2);
+
+        }
+        
+
+        static(int,string) A()
+        {
+            return (1, "salam");
+        }
+
+        static(int,int,int) B()
+        {
+            return (55, 66, 77);
+        }
+
+        static (int, bool, int ,string) C()
+        {
+            return (55, false, 77 ,"AzTU");
+        }
+
+
+    }
+}
